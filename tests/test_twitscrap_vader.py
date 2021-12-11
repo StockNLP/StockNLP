0fb90503dffe7dbc14be63c2dc81a9a9a501cc9a
Moses <mosespv407@gmail.com>
2021-12-11 15:36:28 -0800

    updated test cases

--- tests/test_twitscrap_vader.py
+++ tests/test_twitscrap_vader.py
@@ -13,34 +13,67 @@ from twitter_class import get_score
 from twitter_class import get_subset
 
 class Testtwitter_scrapping_vader(unittest.TestCase):
-def test_smoke(self):
-"""Simple smoke test to make sure function runs"""
-has_numbers('$AMC')
-return
-#    def test_smoke(self):
-#       """Simple smoke test to make sure function runs"""
-#        get_subreddit_column(robinhood, )
-#       return
-#    def test_smoke(self):
-#        """Simple smoke test to make sure function runs"""
-#       cashtags(, )
-#        return
-def test_smoke(self):
-"""Simple smoke test to make sure function runs"""
-flatten_list(['$AMC'],['$AMC is going up'],[2/1/1999])
+       def test_smoke(self):
+           """Simple smoke test to make sure function runs"""
+           add_to_df()
+       return
+
+       def test_smoke(self):
+       """Simple smoke test to make sure function runs"""
+         tick_list()
+       return
+
+       def test_smoke(self):
+           """Simple smoke test to make sure function runs"""
+           get_like_retweet_lst()
+       return
+
+       def test_smoke(self):
+           """Simple smoke test to make sure function runs"""
+           preprocessing()
+       return
+
+      def test_smoke(self):
+          """Simple smoke test to make sure function runs"""
+          vaderScoreGenerator()
+      return
+
+      def test_smoke(self):
+          """Simple smoke test to make sure function runs"""
+          appendToMaster()
+      return
+
+      def test_smoke(self):
+          """Simple smoke test to make sure function runs"""
+          get_score()
+      return
+
+     def test_smoke(self):
+         """Simple smoke test to make sure function runs"""
+         get_subset()
+     return
+
+     def test_to_get_expected_vader_score(self):
+         """oneshot test to make sure the function returns the expected value"""
+         assert np.isclose(get_score(),)
+     return
+
+     def test_to_get_expected_master_ticker(self):
+         """oneshot test to make sure the function returns the expected value"""
+         assert np.isclose(get_subset(),)
+     return
+
+   def test_no_value_passed_to_the_function(self):
+       """edge test to make sure the function throws a TypeError when all the parameters
+          are not passed to the function."""
+       with self.assertRaises(TypeError):
+           add_to_df(lst = , tck = , src = , date = ,like_count = , retweet_count = )
+   return
+
+    def test_value_passed_outside_the_parameter_list(self):
+        """edge test to make sure the function throws a TypeError when tick value passed is
+         outside the following parameters: '$AMC','$GMC','$TSLA','$MSFT','$AAPL','$DOGE','$SHIB',
+         '$ETH','$PROG','$BTC','$ADA','$NKLA','$GOOGL','$RIVN','$FB'."""
+        with self.assertRaises(TypeError):
+            add_to_df(lst = , df = , tck ='$APPL' , src = , date = , like_count = , retweet_count = )
     return
- def test_data_value_passed_is_not_string(self):
-"""test to make sure the function throws a TypeError when parameter value passed to the is not a string type."""
-with self.assertRaises(TypeError):
-has_numbers(55)
-return
-#    def test_smoke(self):
-#       """Simple smoke test to make sure function runs"""
-#        add_to_df(, )
-#       return
-#    def test_smoke(self):
-#       """Simple smoke test to make sure function runs"""
-#       has_special_chars(, )
-#        return
-#   def test_smoke(self):
-#      """Simple smoke test to make sure fucntion runs"""
