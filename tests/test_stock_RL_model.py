5778313726ef0db4c719b3fb13d2ab9faef74b23
Moses <mosespv407@gmail.com>
2021-12-11 19:05:34 -0800

    updated the test cases

---
+++ tests/test_stock_RL_model.py
@@ -0,0 +1,73 @@
+"""tests for the stock_RL_model functions"""
+
+import numpy as np
+import unittest
+
+from stock_RL_Model import act
+from stock_RL_Model import expReplay
+from stock_RL_Model import formatPrice
+from stock_RL_Model import getStockDataVec
+from stock_RL_Model import sigmoid
+from stock_RL_Model import getState
+
+class Test_stock_RL_model(unittest.TestCase):
+     def test_smoke(self):
+         """Simple smoke test to make sure function runs"""
+         act()
+      return
+
+     def test_smoke(self):
+         """Simple smoke test to make sure function runs"""
+         expReplay()
+     return
+
+     def test_smoke(self):
+         """Simple smoke test to make sure function runs"""
+         formatPrice()
+     return
+
+     def test_smoke(self):
+         """Simple smoke test to make sure function runs"""
+         getStockDataVec()
+     return
+
+     def test_smoke(self):
+         """Simple smoke test to make sure function runs"""
+         sigmoid()
+     return
+
+     def test_smoke(self):
+         """Simple smoke test to make sure function runs"""
+         getState()
+     return
+
+     def test_to_get_expected_value(self):
+         """oneshot test to make sure the function returns the expected value"""
+         assert np.isclose(sigmoid(),)
+     return
+
+     def test_to_get_expected_formatprice(self):
+         """oneshot test to make sure the function returns the expected value"""
+         assert np.isclose(formatPrice(),)
+      return
+
+     def test_wrong_number_of_parameters_passed_to_function(self):
+         """edge test to make sure the function throws a TypeError when 
+            correct number of parameter is not passed to the function"""
+         with self.assertRaises(TypeError):                                                                                                                                                getState(t = , n = )
+     return
+
+    def test_check_return_value_of_sigmoid_is_float_type(self):
+        """test to make sure the returned value of the sigmoid function is float type."""
+        self.assertIsInstance(sigmoid(), float)
+    return
+
+    def test_check_return_value_of_getState_is_list_type(self):
+        """test to make sure the returned value of the getState function is list type."""
+        self.assertIsInstance(getState(), list)
+    return
+
+    def test_check_return_value_of_getStockDataVec_is_list_type(self):
+        """test to make sure the returned value of the getStockDataVec function is list type."""
+        self.assertIsInstance(getStockDataVec(), list)
+    return
