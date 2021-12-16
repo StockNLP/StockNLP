a952daf402fe79151c6c11d7be4cd18225e295a4
Moses <mosespv407@gmail.com>
2021-12-16 13:37:05 -0800

    updated

--- tests/test_master_dash.py
+++ tests/test_master_dash.py
@@ -5,42 +5,41 @@ import pandas as pd
 import unittest
 from master_dashboard import movingaverage
 from master_dashboard import get_senti_trend
-from master_dashboard import formatPrice
-from master_dashboard import sigmoid
+from master_dashboard import Agent as ag
 
 class Test_master_dash(unittest.TestCase):
 
-   def test_smoke(self):
-       """Simple smoke test to make sure function runs"""
-       movingaverage([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15], window_size=10)
-       return
+    def test_smoke(self):
+        """Simple smoke test to make sure function runs"""
+        movingaverage([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15], window_size=10)
+        return
 
-   def test_smoke_1(self):
-       """Simple smoke test to make sure function runs"""
-       get_senti_trend(pd.read_csv('./subs.csv'))
-       return
+    def test_smoke_1(self):
+        """Simple smoke test to make sure function runs"""
+        get_senti_trend(pd.read_csv('./subs.csv'))
+        return
 
-   def test_smoke_2(self):
-       """Simple smoke test to make sure function runs"""
-       formatPrice(2.156)
-       return
+    def test_smoke_2(self):
+        """Simple smoke test to make sure function runs"""
+        ag.formatPrice(2.156)
+        return
 
-   def test_smoke_3(self):
-       """Simple smoke test to make sure function runs"""
-       sigmoid(3.68)
-       return
+    def test_smoke_3(self):
+        """Simple smoke test to make sure function runs"""
+        ag.sigmoid(3.68)
+        return
 
-   def test_to_get_expected_sigmoid_value(self):
-       """oneshot test to make sure the function returns the expected value"""
-       assert np.isclose(sigmoid(3.68),0.97539)
-       return
+    def test_to_get_expected_sigmoid_value(self):
+        """oneshot test to make sure the function returns the expected value"""
+        assert np.isclose(ag.sigmoid(3.68),0.97539)
+        return
 
-  def test_to_get_expected_formatprice(self):
-     """oneshot test to make sure the function returns the expected value"""
-     assert np.isclose(formatPrice(2.156), '$2.15')
-     return
+    def test_to_get_expected_formatprice(self):
+        """oneshot test to make sure the function returns the expected value"""
+        assert np.isclose(ag.formatPrice(2.156), '$2.15')
+        return
 
-  def test_check_return_value_of_sigmoid_is_float_type(self):
-      """test to make sure the returned value of the sigmoid function is float type."""
-      self.assertIsInstance(sigmoid(3.68), float)
-      return
+    def test_check_return_value_of_sigmoid_is_float_type(self):
+        """test to make sure the returned value of the sigmoid function is float type."""
+        self.assertIsInstance(ag.sigmoid(3.68), float)
+        return
