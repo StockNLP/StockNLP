a5802ba3f7e362bd096b6c33aa24c1f7bbf1adef
Moses <mosespv407@gmail.com>
2021-12-16 12:52:49 -0800

    updated testcases

--- tests/test_master_dash.py
+++ tests/test_master_dash.py
@@ -1,73 +1,46 @@
-"""tests for the master_dash functions"""
+"""tests for the master_dashboard functions"""
 
 import numpy as np
+import pandas as pd
 import unittest
-
-from master_dash import get_candlestick_chart
-from master_dash import movingaverage
-from master_dash import bbands
-from master_dash import get_reddit_data
-from master_dash import get_senti_trend
+from master_dashboard import movingaverage
+from master_dashboard import get_senti_trend
+from master_dashboard import formatPrice
+from master_dashboard import sigmoid
 
 class Test_master_dash(unittest.TestCase):
-    def test_smoke(self):
-        """Simple smoke test to make sure function runs"""
-        get_candlestick_chart()
-    return
 
    def test_smoke(self):
        """Simple smoke test to make sure function runs"""
-       movingaverage()
-   return
+       movingaverage([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15], window_size=10)
+       return
 
-   def test_smoke(self):
+   def test_smoke_1(self):
        """Simple smoke test to make sure function runs"""
-       bbands()
-   return
+       get_senti_trend(pd.read_csv('./subs.csv'))
+       return
 
-   def test_smoke(self):
+   def test_smoke_2(self):
        """Simple smoke test to make sure function runs"""
-       get_reddit_data()
-   return
+       formatPrice(2.156)
+       return
 
-   def test_smoke(self):
+   def test_smoke_3(self):
        """Simple smoke test to make sure function runs"""
-         get_senti_trend()
-   return
+       sigmoid(3.68)
+       return
 
-   def test_to_get_expected_bbands(self):
+   def test_to_get_expected_sigmoid_value(self):
        """oneshot test to make sure the function returns the expected value"""
-       assert np.isclose(bbands(),)
-   return
+       assert np.isclose(sigmoid(3.68),0.97539)
+       return
 
-   def test_to_get_expected_master_ticker(self):
-       """oneshot test to make sure the function returns the expected value"""
-       assert np.isclose(get_subset(),)
-   return
+  def test_to_get_expected_formatprice(self):
+     """oneshot test to make sure the function returns the expected value"""
+     assert np.isclose(formatPrice(2.156), '$2.15')
+     return
 
-   def test_wrong_value_passed_as_the_windows(self):
-       """edge test to make sure the function throws a TypeError when windows value
-          is not equal to 10"""
-       with self.assertRaises(TypeError):                                                                                                                                                movingaverage(interval = , windows = 7)
-   return
-
-   def test_wrong_value_passed_as_the_window_size(self):
-       """edge test to make sure the function throws a TypeError when windows value
-          is not equal to 10"""
-       with self.assertRaises(TypeError):
-           bbands(price = , window_size = 12, num_of_std=5)
-    return
-
-   def test_wrong_value_passed_as_the_num_of_std(self):
-       """edge test to make sure the function throws a TypeError when num_of_std value
-          is not equal to 5"""
-       with self.assertRaises(TypeError):
-           bbands(price = , window_size = 10, num_of_std=8)
-   return
-
-   def test_wrong_value_passed_as_the_window_size_num_of_std(self):
-       """edge test to make sure the function throws a TypeError when windows value
-          is not equal to 10 and num_of_std is not equal to 5"""
-       with self.assertRaises(TypeError):
-           bbands(price = , window_size = 12, num_of_std=4)
-   return
+  def test_check_return_value_of_sigmoid_is_float_type(self):
+      """test to make sure the returned value of the sigmoid function is float type."""
+      self.assertIsInstance(sigmoid(3.68), float)
+      return
