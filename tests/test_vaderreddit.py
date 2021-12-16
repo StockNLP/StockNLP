5ca7f608ff4247ae8aa914832846cac98894fcbe
achalla123 <36082388+achalla123@users.noreply.github.com>
2021-12-16 01:18:11 -0500

    Fixed indentation and code issues

--- tests/test_vaderreddit.py
+++ tests/test_vaderreddit.py
@@ -11,71 +11,71 @@ from vader_reddit import get_subset
 from vader_reddit import get_score
 
 class Test_vader_reddit_score(unittest.TestCase):
-      def test_smoke(self):
-          """Simple smoke test to make sure function runs"""
-          add_vader_scores()
-     return
-     
-     def test_smoke(self):
-         """Simple smoke test to make sure function runs"""
-         add_vader_weighted_sentiments()
-     return
 
-     def test_smoke(self):
+    def test_smoke(self):
+        """Simple smoke test to make sure function runs"""
+        add_vader_scores()
+        return
+
+    def test_smoke(self):
+        """Simple smoke test to make sure function runs"""
+        add_vader_weighted_sentiments()
+        return
+
+    def test_smoke(self):
         """Simple smoke test to make sure function runs"""
         get_subset(,)
-     return
+        return
 
     def test_smoke(self):
         """Simple smoke test to make sure function runs"""
         get_score()
-    return
+        return
 
     def test_dataframe_equal(self):
-       """edge test to make sure dataframe is as expected"""
-       pandas.testing.assert_frame_equal(add_vader_scores(), )
-    return
+        """edge test to make sure dataframe is as expected"""
+        pandas.testing.assert_frame_equal(add_vader_scores(), )
+        return
 
     def test_dataframe_equal(self):
         """edge test to make sure dataframe is as expected"""
         pandas.testing.assert_frame_equal(add_vader_weighted_sentiments(), )
-    return
+        return
 
     def test_dataframe_equal(self):
         """edge test to make sure dataframe is as expected"""
         pandas.testing.assert_frame_equal(get_subset(), )
-    return
+        return
 
     def test_dataframe_equal(self):
         """edge test to make sure dataframe is as expected"""
         pandas.testing.assert_frame_equal(get_score(), )
-    return
+        return
 
     def test_no_value_passed_to_the_function(self):
         """edge test to make sure the function throws a TypeError when all the parameters
            are not passed to the function."""
         with self.assertRaises(TypeError):
-            get_subset(stock_name = )
-     return
+            get_subset(stock_name=)
+        return
 
-     def test_no_value_passed_to_the_function(self):
-         """edge test to make sure the function throws a TypeError when all the parameters
-            are not passed to the function."""
-          with self.assertRaises(TypeError):
-              get_subset(df = )
-      return
-
-      def test_value_passed_outside_the_parameter_list(self):
-          """edge test to make sure the function throws a TypeError when stock_name passed is
-             not string type"""
-           with self.assertRaises(TypeError):
-               get_subset(stock_name = 55, df = )
-       return
-
-   def test_value_passed_outside_the_parameter_list(self):
-       """edge test to make sure the function throws a TypeError when stock_name passed is
-          not in the list of stock names"""
+    def test_no_value_passed_to_the_function(self):
+        """edge test to make sure the function throws a TypeError when all the parameters
+           are not passed to the function."""
         with self.assertRaises(TypeError):
-            get_subset(stock_name = '$HHY', df = )
-    return
+            get_subset(df=)
+        return
 
+    def test_value_passed_outside_the_parameter_list(self):
+        """edge test to make sure the function throws a TypeError when stock_name passed is
+           not string type"""
+        with self.assertRaises(TypeError):
+            get_subset(stock_name=55, df=)
+        return
+
+    def test_value_passed_outside_the_parameter_list(self):
+        """edge test to make sure the function throws a TypeError when stock_name passed is
+           not in the list of stock names"""
+        with self.assertRaises(TypeError):
+            get_subset(stock_name='$HHY', df=)
+        return
